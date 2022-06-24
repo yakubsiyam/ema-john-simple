d1fa352a44ac4d0f912be0669fb5a58c8ff75490
Siyam <itsyakubsiyam@gmail.com>
2022-06-24 23:49:53 +0600

    cart calculation and showing its into UI

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,24 @@
 import React from "react";
 
 const Cart = ({ cart }) => {
+  let total = 0;
+  let shipping = 0;
+
+  for (const product of cart) {
+    total = total + product.price;
+    shipping = shipping + product.shipping;
+  }
+
+  const tax = parseFloat((total * 0.1).toFixed(2));
+  const grandTotal = total + shipping + tax;
   return (
-    <div>
-      <h3 className="mt-5 text-center">Order Summary</h3>
-      <p>{cart.length}</p>
+    <div className=" sticky-top px-5">
+      <h3 className="mt-5">Order Summary</h3>
+      <p>Selected Items: {cart.length}</p>
+      <p>Total price: $ {total}</p>
+      <p>Total Shipping: $ {shipping}</p>
+      <p>Tax: ${tax}</p>
+      <h5>Grand Total: $ {grandTotal}</h5>
     </div>
   );
 };
